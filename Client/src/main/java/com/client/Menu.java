bbad3614bd4d7ffb2fb199c7c83623fceba929da
Brumocas <brucosta2002@gmail.com>
2023-10-31 20:02:26 +0000

    lab1

--- Client/src/main/java/com/client/Menu.java
+++ Client/src/main/java/com/client/Menu.java
@@ -14,13 +14,6 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class Menu {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 
     @FXML
     private Button newGameButton;
