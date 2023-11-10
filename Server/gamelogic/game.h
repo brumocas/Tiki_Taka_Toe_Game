110bca5492ec27d5d703556b1b1b8442b2c9b303
FmesquitaUP <up201806489@up.pt>
2023-11-10 19:10:22 +0000

    Game class

--- Server/gamelogic/game.h
+++ Server/gamelogic/game.h
@@ -5,10 +5,29 @@
 #ifndef SERVER_GAME_H
 #define SERVER_GAME_H
 
+#include "player.h"
+#include <array>
+#include <iostream>
 
-class game {
+using ThreeByThreeArray = std::array<std::array<int, 3>, 3>;
 
-};
+namespace gamelogic {
 
+    class game {
+    private:
+        Player player1, player2, currentPlayer, winner;
+
+    public:
+
+        bool checkPosition(Player player);
+        bool checkAnswer(Player player);
+        Player getWinner();
+        void setWinner(Player player);
+        bool isGameWonByPlayer(Player player);
+        bool isGameTied();
+        std::array<ThreeByThreeArray, 8> winningCombination(char symbol);
+
+    };
+}
 
 #endif //SERVER_GAME_H
