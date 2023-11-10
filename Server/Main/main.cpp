1acbd0adb1da037e79eadaaa7a96e2d0cdbb516a
FmesquitaUP <up201806489@up.pt>
2023-11-10 16:46:57 +0000

    Player CLass

---
+++ Server/Main/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by fmesquita on 11/10/23.
+//
+#include "../gamelogic/board.h"
+#include "../gamelogic/game.h"
+#include "../gamelogic/gamerunner.h"
+#include "../gamelogic/player.h"
+#include "../communication//communication.h"
+#include "../database/database.h"
+#include "../database/footballer.h"
+
+#include <iostream>
