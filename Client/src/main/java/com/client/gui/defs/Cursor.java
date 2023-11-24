de4123ac7f997ac38f7a0c83197b30f17854e691
Brumocas <brucosta2002@gmail.com>
2023-11-24 09:48:17 +0000

    Image path correction

--- Client/src/main/java/com/client/gui/defs/Cursor.java
+++ Client/src/main/java/com/client/gui/defs/Cursor.java
@@ -9,7 +9,7 @@ public class Cursor {
     public static void setCursor(Scene scene) {
 
         // Load a custom cursor image
-        Image cursorImage = new Image("file:iconball.png");
+        Image cursorImage = new Image("file:src/main/images/iconball.png");
 
         // Create a custom cursor with the image and hotspot
         ImageCursor customCursor = new ImageCursor(cursorImage, cursorImage.getWidth() / 1.75, cursorImage.getHeight() / 1.8);
